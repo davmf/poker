ba0378ea5bce4a7ca9d6b1624a756b787951208f
David Fyfe <david@davidfyfe.net>
2023-04-01 22:11:52 +1100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,27 @@
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
     unimplemented!("Out of {hands:?}, which hand wins?")
 }
+
+struct Card {
+    value: char,
+    suit: char,
+}
+
+impl Card {
+    fn new(value: char, suit: char) -> Result<Card> {
+        let values = vec!["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+        let suits = vec!["C", "S", "D", "H"];
+        
+        if value in ['A', '2', '3', '4', '5', '6', '7', '8', '9']
+    };
+    Ok(Card{ value, suit};)
+}
+
+struct PokerHand {
+    cards: Vec<Card>,
+}
+
+impl PokerHand {
+    fn new(hand: &str) -> Result<PokerHand> {
+
+}
