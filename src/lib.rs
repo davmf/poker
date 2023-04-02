7a0bb07ee9a6bec88fe71db652c742adc22ea082
David Fyfe <david@davidfyfe.net>
2023-04-02 22:23:10 +1000

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,22 @@
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
+use std::cmp::Ordering;
+
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
     unimplemented!("Out of {hands:?}, which hand wins?")
+}
+
+enum PokerHandType {
+    StraightFlush(),
+    FourOfAKind,
+    FullHouse,
+    Flush,
+    Straight,
+    ThreeOfAKind,
+    TwoPair,
+    OnePair,
+    HighCard,
 }
 
 struct Card {
@@ -14,7 +28,10 @@ struct Card {
 impl Card {
     fn new(card: &str) -> Result<Card, String> {
         let values: Vec<String> = 
-            vec!["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"].iter().map(|c| c.to_string()).collect();
+            vec!["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+                .iter()
+                .map(|c| c.to_string())
+                .collect();
 
         let suits: Vec<String> = vec!["C", "S", "D", "H"].iter().map(|c| c.to_string()).collect();
         let value = card[..card.len()-1].to_string();
@@ -27,12 +44,18 @@ impl Card {
     }
 }
 
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 struct PokerHand {
     cards: Vec<Card>,
 }
 
 impl PokerHand {
-    fn new(hand: String) -> Result<PokerHand, String> {
+    fn new(hand: &str) -> Result<PokerHand, String> {
         let card_strings: Vec<String> =
             hand
             .split_ascii_whitespace()
@@ -43,52 +66,52 @@ impl PokerHand {
             return Err("Invalid length".to_string())
         }
 
-        // let mut cards = vec![];
-
-        // for card_string in &card_strings {
-        //     cards.push(Card::new(card_string).unwrap());
-        // }
         let cards = card_strings
-        .iter()
-        .map(|card_string| Card::new(card_string))
-        .collect::<Result<Vec<Card>, _>>()?;
-    
-        // let cards: Vec<Card> = card_strings
-        //     .iter()
-        //     .map(|card_string| Card::new(card_string)?)
-        //     .collect();
+            .iter()
+            .map(|card_string| Card::new(card_string))
+            .collect::<Result<Vec<Card>, _>>()?;   
     
         Ok( PokerHand { cards })
-        // let cards: Vec<Result<Card, &str> =
-        //     card_strings
-        //     .iter()
-        //     .map(|cs| Card::new(cs).unwrap())
-        //     .collect();
+    }
+
+    fn is_straight_flush(&self) {
+
     }
 }
 
-!cfg[test]
-{
-    
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
     #[test]
     fn test_new_card() {
-        let card = &Card::new("2C").unwrap();
+        let card = Card::new("2C").unwrap();
         assert!(card.value == "2");
         assert!(card.suit == "C");
     }
 
     #[test]
     fn test_another_new_card() {
-        let card = &Card::new("10D").unwrap();
+        let card = Card::new("10D").unwrap();
         assert!(card.value == "10");
         assert!(card.suit == "D");
     }
 
     #[test]
     fn test_bad_card() {
-        let card = &Card::new("11S");
+        let card = Card::new("11S");
         assert!(card.is_err());
+    }
+
+    #[test]
+    fn test_poker_hand() {
+        let hand = PokerHand::new("2C 3D 4H 5C 8S");
+        assert!(hand.is_ok());
+    }
+
+    #[test]
+    fn test_bad_poker_hand() {
+        let hand = PokerHand::new("2C 3X 4H 5C 8S");
+        assert!(hand.is_err());
     }
 }
